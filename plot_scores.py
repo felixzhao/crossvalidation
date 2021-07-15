6f3322e45aa83af8850948ac5d3328bf7f6b57b5
Felix Zhao <felixz2010@gmail.com>
2021-07-16 09:13:12 +1200

    add scores plot.

---
+++ plot_scores.py
@@ -0,0 +1,30 @@
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn import svm
+from sklearn.model_selection import cross_validate
+
+# load data
+X, y = datasets.load_iris(return_X_y=True)
+print(X.shape, y.shape)
+
+# split train test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=0)
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
+clf = svm.SVC(kernel='linear', C=1).fit(X_train, y_train)
+
+# cross validation
+scoring = ['precision_macro', 'recall_macro']
+scores = cross_validate(clf, X_train, y_train, scoring=scoring)
+print(f'cross validation:')
+print(f'score keys:\n{sorted(scores.keys())}')
+print(f"test_recall_macro scores:\n{scores['test_recall_macro']}")
+print()
+
+score_name = f'test_{scoring[0]}'
+plt.plot(scores[score_name])
+plt.xlabel("cv")
+plt.ylabel(score_name)
+plt.legend([score_name])
+plt.show()
